0e19b30094a59b19064374cc60b89001b770a491
Maya BAOUCHE <iloveprogramming112@gmail.com>
2019-11-02 15:20:53 +0100

    Add EpisodeManager class definition

--- Model/Episodes/EpisodeManager.cs
+++ Model/Episodes/EpisodeManager.cs
@@ -8,5 +8,32 @@ namespace cnam_mania.Model.Episodes
 {
     class EpisodeManager
     {
+        /// <summary>
+        /// Current Episode id.
+        /// </summary>
+        private int CurrentEpisode { get; set; }
+
+        /// <summary>
+        /// Next episode id.
+        /// </summary>
+        private int NextEpisode { get; set; }
+
+        /// <summary>
+        /// List of available episodes. 
+        /// </summary>
+        private List<Episode> Episodes { get; set; }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="currentEpisode"></param>
+        /// <param name="nextEpisode"></param>
+        /// <param name="episodes"></param>
+        public EpisodeManager(int currentEpisode, int nextEpisode, List<Episode> episodes)
+        {
+            CurrentEpisode = currentEpisode;
+            NextEpisode = nextEpisode;
+            Episodes = episodes;
+        }
     }
 }
