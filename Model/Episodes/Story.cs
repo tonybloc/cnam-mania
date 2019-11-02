4ee9e464f28dcd4bc383cae2ed8d9fe4bc5f1fbd
Maya BAOUCHE <iloveprogramming112@gmail.com>
2019-11-02 15:31:54 +0100

    Add Story class structure

--- Model/Episodes/Story.cs
+++ Model/Episodes/Story.cs
@@ -8,5 +8,32 @@ namespace cnam_mania.Model.Episodes
 {
     class Story
     {
+        /// <summary>
+        /// Current choice made.
+        /// </summary>
+        private Choice CurrentChoice { get; set; }
+
+        /// <summary>
+        /// Choices available for a story.
+        /// </summary>
+        private List<Choice> Choices { get; set; }
+
+        /// <summary>
+        /// Defines if a story is crucial or not.
+        /// </summary>
+        private bool IsCrucial { get; set; }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="currentChoice"></param>
+        /// <param name="choices"></param>
+        /// <param name="isCrucial"></param>
+        public Story(Choice currentChoice, List<Choice> choices, bool isCrucial)
+        {
+            CurrentChoice = currentChoice;
+            Choices = choices;
+            IsCrucial = isCrucial;
+        }
     }
 }
