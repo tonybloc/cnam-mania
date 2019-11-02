661ec6e06d54323ecdb3111e41643eed6de41beb
Bruno Malvy <Bruno@MacBook-Pro-de-Bruno.local>
2019-11-02 15:27:01 +0100

    feat(): Creat Class MiniGames

---
+++ Model/MiniGames/MiniGamesFactory.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnam_mania.Model.MiniGames
+{
+public class MiniGamesFactory
+    {
+        public AbsGameManeger createGamesManager(string game)
+        {
+        }
+    }
+}
---
+++ Model/MiniGames/RiddleManager.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnam_mania.Model.MiniGames
+{
+    public class RiddleManager : AbsGameManager
+    {
+    }
+    public void Play()
+ {
+ }
+}
