7122db15cdc2b93338f7d3d5bcab709c44be8c71
Maya BAOUCHE <iloveprogramming112@gmail.com>
2019-11-06 20:54:49 +0100

    Add initGame implementation

--- VisualNovelGame/Manager/VisualNovelManager.cs
+++ VisualNovelGame/Manager/VisualNovelManager.cs
@@ -1,10 +1,13 @@
 ﻿using cnam_mania.Game;
 using cnam_mania.VisualNovelGame.Manager.Episode;
+using cnam_mania.VisualNovelGame.Model.Levels;
+using cnam_mania.VisualNovelGame.Model.Characters; 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using cnam_mania.VisualNovelGame.Model.Episodes;
 
 namespace cnam_mania.VisualNovelGame.Manager
 {
@@ -22,11 +25,22 @@ namespace cnam_mania.VisualNovelGame.Manager
         public EpisodeManager episodeManager;
 
         /// <summary>
+        /// CharacterManager instance
+        /// </summary>
+        public CharacterManager characterManager;
+
+        /// <summary>
+        /// Game mode choosen by the user
+        /// </summary>
+        public IModeStrategy GameMode { get; set; }
+
+        /// <summary>
         /// Creation of the VisualNovelManager
         /// </summary>
         private VisualNovelManager()
         {
             this.episodeManager = EpisodeManager.Instance;
+            this.characterManager = CharacterManager.Instance; 
         }
 
         /// <summary>
@@ -57,5 +71,69 @@ namespace cnam_mania.VisualNovelGame.Manager
         {
 
         }
+
+        /// <summary>
+        /// Initialize the game items. 
+        /// </summary>
+        public void InitGame(String character, String gameMode)
+        {
+             // chooses the Character according to user's choice
+            switch (character)
+            {
+                case "SMART":
+                    this.characterManager.SetCharacterBuilder(new SmartCharacterBuilder());
+                    break;
+                case "GREEDY":
+                    this.characterManager.SetCharacterBuilder(new GreedyCharacterBuilder());
+                    break;
+                case "POPULAR":
+                    this.characterManager.SetCharacterBuilder(new PopularCharacterBuilder());
+                    break;
+                case "RICH":
+                    this.characterManager.SetCharacterBuilder(new RichCharacterBuilder());
+                    break;
+                default:
+                    break; 
+
+            }
+           
+            // chooses the game mode according to user's choice
+            switch (gameMode)
+            {
+                case "EASY":
+                    this.GameMode = new EasyMode();
+                    break;
+                case "MEDIUM":
+                    this.GameMode = new MediumMode();
+                    break;
+                case "HARD":
+                    this.GameMode = new HardMode();
+                    break;
+                default:
+                    break; 
+            }
+        }
+
+        public void SaveGame()
+        {
+
+        }
+
+        public void CreateCharacter()
+        {
+            // TODO 
+
+        }
+
+        public void CheckVictory()
+        {
+            // TODO 
+        }
+
+        public void SwitchEpisode()
+        {
+            // TODO 
+        }
+
     }
 }
