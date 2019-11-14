36560682b12a81727125c8d35c22d8c2602e7e9c
Maya BAOUCHE <iloveprogramming112@gmail.com>
2019-11-14 14:12:49 +0100

    Add Fun facts generator

---
+++ RiddleGame/FactManager.cs
@@ -0,0 +1,96 @@
+﻿using cnam_mania.Game;
+using cnam_mania.RiddleGame.Model;
+using cnam_mania.Settings;
+using cnam_mania.VisualNovelGame.Service.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnam_mania.RiddleGame
+{
+    public class FactManager : AbsGameManager
+    {
+        // Unique instance of manager
+        private static FactManager _instance;
+
+
+        #region Variables
+
+        #region Bindable Attributes
+        public Facts Facts
+        {
+            get { return _facts; }
+            set
+            {
+                if (_facts != value)
+                {
+                    _facts = value;
+                }
+            }
+        }
+        public Fact CurrentFact
+        {
+            get { return _fact; }
+            set
+            {
+                if (_fact != value)
+                {
+                    _fact = value;
+                }
+            }
+        }
+        #endregion 
+
+
+        #region Variable (private)
+        private Facts _facts { get; set; }
+        private Fact _fact { get; set; }
+        #endregion
+        #endregion
+
+
+        #region Constructor / Instance
+
+        /// <summary>
+        /// Create new instance of EpisodeManager
+        /// </summary>
+        private FactManager()
+        {
+            try
+            {
+                this.Facts = XmlDataAccess.XMLDeserializeObject<Facts>(Config.filePath_XmlFacts);
+                this.CurrentFact = this.Facts.Stereotypes[0];
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(string.Format("Une erreur est survenu lors de la déserialisation ! {0}", exp.Message));
+                this.CurrentFact = null;
+            }
+
+        }
+
+        /// <summary>
+        /// Get unique instance of manager
+        /// </summary>
+        public static FactManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new FactManager();
+                }
+                return _instance;
+            }
+        }
+
+        #endregion
+        public override void Play()
+        {
+
+        }
+        
+    }
+}
--- VisualNovelGame/View/StereotypesInterface.xaml.cs
+++ VisualNovelGame/View/StereotypesInterface.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using cnam_mania.RiddleGame;
+using cnam_mania.RiddleGame.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +22,92 @@ namespace cnam_mania.VisualNovelGame.View
     /// <summary>
     /// Logique d'interaction pour StereotypesInterface.xaml
     /// </summary>
-    public partial class StereotypesInterface : Page
+    public partial class StereotypesInterface : Page, INotifyPropertyChanged
     {
+
+        /// <summary>
+        /// current story
+        /// </summary>
+        private Fact _fact;
+
+        private int currentIndex = 0; 
+
+        /// <summary>
+        /// Facts manager
+        /// </summary>
+        private FactManager _factManager; 
+
+        // Property change attribute
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// current fact
+        /// </summary>
+        public Fact Fact
+        {
+            get { return _fact; }
+            set
+            {
+                if (_fact != value)
+                {
+                    _fact = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
         public StereotypesInterface()
         {
             InitializeComponent();
+            DataContext = this;
+            
+            _factManager = FactManager.Instance;
+            Shuffle(this._factManager.Facts.Stereotypes);
+            SetCurrentFact(this._factManager);
+            
+        }
+
+        /// <summary>
+        /// Initialize current fact
+        /// </summary>
+        /// <param name="manager"></param>
+        public void SetCurrentFact(FactManager manager)
+        {
+            this._fact = manager.Facts.Stereotypes.ElementAt(0);
+        }
+
+
+        /// <summary>
+        /// Shuffle method
+        /// </summary>
+        public static void Shuffle<T>(List<T> list)
+        {
+            Random rand = new Random();
+            int nbMotion = list.Count;
+            while (nbMotion > 1)
+            {
+                nbMotion--;
+                int randomIndex = rand.Next(nbMotion + 1);
+                T obj = list[randomIndex];
+                list[randomIndex] = list[nbMotion];
+                list[nbMotion] = obj;
+            }
+
+        }
+
+        /// <summary>
+        /// Generate random facts 
+        /// </summary>
+        public void GenerateRandomFact(object sender, RoutedEventArgs e)
+        {
+            if (this.currentIndex == this._factManager.Facts.Stereotypes.Count-1)
+                this.currentIndex = 0;
+            else
+                this.currentIndex++;
+            this.Fact = this._factManager.Facts.Stereotypes.ElementAt(this.currentIndex); 
         }
 
         /// <summary>
@@ -36,5 +121,17 @@ namespace cnam_mania.VisualNovelGame.View
             ((MainWindow)Window.GetWindow(this)).MenuContent.Visibility = Visibility.Visible;
             this.Content = null;
         }
+
+
+        #region Notify Bindable attribute
+        /// <summary>
+        /// Notify property changed
+        /// </summary>
+        /// <param name="propertyName">Property name</param>
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
     }
 }
